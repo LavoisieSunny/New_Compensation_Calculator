226f972503e08977ce0923cfb0b190863d00fddd
LavoisieSunny <lavoisiesunnycourse@gmail.com>
2026-05-28 12:02:40 +0530

    Update default host to 0.0.0.0 and port to 8050 for remote network server bindings

--- config/ports.py
+++ config/ports.py
@@ -8,11 +8,11 @@ import os
 # Host address to bind the server to.
 # '127.0.0.1' is for local testing.
 # '0.0.0.0' allows external connections (useful for cloud/dedicated servers).
-HOST = os.getenv("COMP_CALC_HOST", "127.0.0.1")
+HOST = os.getenv("COMP_CALC_HOST", "0.0.0.0")
 
 # Port to run the application on.
-# Default is 8080. You can easily change this to 80, 443, or any other port.
-PORT = int(os.getenv("COMP_CALC_PORT", 8080))
+# Default is 8050. You can easily change this to 80, 443, or any other port.
+PORT = int(os.getenv("COMP_CALC_PORT", 8050))
 
 # Debug mode for FastAPI (automatic reload, interactive docs)
 DEBUG = os.getenv("COMP_CALC_DEBUG", "True").lower() == "true"
